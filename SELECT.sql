0ad9b8f5b54ae35d338473b94b39c90a8a05be80
AdiaEra <121941801+AdiaEra@users.noreply.github.com>
2023-03-14 21:31:06 +0300

    change_2

--- SELECT.sql
+++ SELECT.sql
@@ -5,24 +5,24 @@ WHERE year_of_release = 2018;
 --2 название и продолжительность самого длительного трека:
 SELECT title, duration
 FROM track
-WHERE duration = (SELECT MAX(duration) FROM track)
+WHERE duration = (SELECT MAX(duration) FROM track);
 
 --или так:
 SELECT title, duration FROM track
 ORDER BY duration DESC
-LIMIT 1
+LIMIT 1;
 
 --3 название треков, продолжительность которых не менее 3,5 минуты:
 SELECT title FROM track
-WHERE duration >= 210
+WHERE duration >= 210;
 
 --4 названия сборников, вышедших в период с 2018 по 2020 год включительно:
 SELECT title FROM collection
-WHERE year_of_release BETWEEN 2018 AND 2020
+WHERE year_of_release BETWEEN 2018 AND 2020;
 
 --5 исполнители, чье имя состоит из 1 слова:
 SELECT name_nickname FROM performer
-WHERE name_nickname NOT LIKE '% %'
+WHERE name_nickname NOT LIKE '% %';
 
 --6 название треков, которые содержат слово «мой» или «my»:
 SELECT title FROM track
